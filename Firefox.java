d14810329e31788fe20f7018b69f5136701ccae4
preeethirao <108994644+preeethirao@users.noreply.github.com>
2022-07-09 22:55:39 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float version = 100.2f;
+		String name = "firefox";
+		boolean visible = true;
+		System.out.println("name" +name);
+		System.out.println(version);
+	}
+
+}
